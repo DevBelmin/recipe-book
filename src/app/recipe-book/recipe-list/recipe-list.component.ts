33d82a8aba518c8a0acc7b27e73d20ce6fa18b2f
devbelmin <sabic.belmin@gmail.com>
2018-08-11 15:20:59 +0200

    Created recipe  item component and used in in the recipe list component

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -9,7 +9,10 @@ import { Recipe } from '../models/recipe';
 export class RecipeListComponent implements OnInit {
 
    recipes: Recipe[] = [
-     new Recipe('Test Recipe', 'This is a test description', 'https://pixabay.com/en/dishes-kitchen-bio-food-recipe-1175493/')
+     new Recipe('Test Recipe 1', 'This is a test description 1', 'https://c1.staticflickr.com/6/5737/30622968353_35e06fcb52_b.jpg'),
+     new Recipe('Test Recipe 2', 'This is a test description 2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnrnfL-aZYmCr0APAF4mmnW5qa90bzt4ZYb6c-vSetaXJxUZ3z'),
+     new Recipe('Test Recipe 3', 'This is a test description 3', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS89bBewZrHco39iq9isJGMT-SqMdakr5lTGG3qfMJYNsenvJkc-w'),
+     new Recipe('Test Recipe 4', 'This is a test description 4', 'https://upload.wikimedia.org/wikipedia/commons/e/e9/Egyptian_food_Koshary.jpg')
    ];
 
   constructor() { }
